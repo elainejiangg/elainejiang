3e0b2c327a7664429a469e4bb16cbf26dc4f4954
elaine jiang <ejian@MacBook-Pro-4.local>
2024-06-22 18:37:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      {/* NAVBAR */}
+      <header className="header">
+        <a href="#home">elaine jiang.</a>
+        <nav className="flex justify-between space-x-4 underline">
+          <a href="#projects">projects</a>
+          <a href="#skills">skills</a>
+          <a href="#resume">resume</a>
+          <a href="#contact">contact</a>
+        </nav>
       </header>
-    </div>
+
+      <h1 id="home" className="text-3xl font-bold">
+        elaine jiang.
+      </h1>
+      <h2 className="font-bold">aspiring software engineer</h2>
+
+      <h2 className="font-bold">about me</h2>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+
+      <h2 className="font-bold ">let's connect!</h2>
+      <div className="flex row space-x-2">
+        <a href="mailto:ejiang@mit.edu">
+          <img src="assets/mail.png" alt="mail icon"></img>
+        </a>
+        <a href="https://www.linkedin.com/in/elainemjiang/">
+          <img src="assets/linkedin.png" alt="linkedin icon"></img>
+        </a>
+        <a href="https://github.com/elainejiangg">
+          <img src="assets/github.png" alt="github icon"></img>
+        </a>
+      </div>
+
+      <h2 id="projects" className="font-bold">
+        projects
+      </h2>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <h2 id="resume" className="font-bold">
+        resume
+      </h2>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+
+      <h2 id="skills" className="font-bold">
+        skills
+      </h2>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <h2 id="contact" className="font-bold">
+        contact
+      </h2>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+      <p>
+        hello! I’m Elaine. I’m currently a sophomore at MIT majoring in 6-4, AI
+        & Decision-Making. I’m interested in AI, specifically computer vision
+        and NLP, and full-stack development. I’m born and raised in Queens, NYC,
+        but currently based in Cambridge, MA!{" "}
+      </p>
+    </>
   );
-}
+};
 
 export default App;
